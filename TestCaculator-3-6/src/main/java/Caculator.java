7b6e1485f98c4f4a753a36c80390c09db9d51d0d
luult_54 <luulethe@gmail.com>
2013-06-03 14:14:04 +0700

    commit code 3-6 (over 5 minutes)

---
+++ TestCaculator-3-6/src/main/java/Caculator.java
@@ -0,0 +1,67 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: luu
+ * Date: 6/3/13
+ * Time: 1:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Caculator {
+    public static int add(String text) throws Exception {
+        if (text.equals(""))
+            return 0;
+        String delimiterString = getDelemiter(text);
+        String[] listNumber = getListNumber(text, delimiterString);
+        return sum(listNumber);
+    }
+
+    private static int sum(String[] listNumber) throws Exception {
+        int sum = 0;
+        String negativeString = "";
+        for (String s : listNumber) {
+            if (Integer.parseInt(s) < 0)
+                negativeString += " " + s;
+            if (Integer.parseInt(s) <= 1000)
+                sum += Integer.parseInt(s);
+        }
+        if (!negativeString.equals(""))
+            throw new Exception("negatives not allowed" + negativeString);
+        return sum;
+    }
+
+    private static String[] getListNumber(String text, String delimiterString) {
+        String numberString = text;
+
+        if (text.startsWith("//")) {
+            numberString = text.split("\n")[1];
+        }
+        return numberString.split(delimiterString);
+    }
+
+    private static String getDelemiter(String text) {
+
+        String delimiterString = "\n|,";
+        if (!text.startsWith("//")) {
+            return delimiterString;
+        }
+        if (!text.startsWith("//[")) {
+            delimiterString = "\n|" + text.substring(2, 3);
+        } else {
+            String[] listDelimiter = text.split("\n")[0].split("]");
+            delimiterString = nomalize(listDelimiter[0].split("\\[")[1]);
+            int i = 0;
+            for (String s : listDelimiter) {
+                i++;
+                if (i > 1)
+                    delimiterString += "|" + nomalize(s.split("\\[")[1]);
+            }
+
+        }
+        return delimiterString;
+    }
+
+    private static String nomalize(String s) {
+        s = s.replaceAll("[*]", "\\\\*");
+        s = s.replaceAll("[?]", "\\\\?");
+        return s.replaceAll("[+]", "\\\\+");
+    }
+}
---
+++ TestCaculator-3-6/src/test/java/TestCaculator.java
@@ -0,0 +1,83 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luu
+ * Date: 6/3/13
+ * Time: 1:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestCaculator {
+    @Test
+    public void testEmptyString() throws Exception {
+        assertEquals(0, Caculator.add(""));
+    }
+
+    @Test
+    public void TestOneNumber() throws Exception {
+        assertEquals(1, Caculator.add("1"));
+    }
+
+    @Test
+    public void TestTwoNumber() throws Exception {
+        assertEquals(3, Caculator.add("1,2"));
+    }
+
+    @Test
+    public void TestUnknownNumber() throws Exception {
+        assertEquals(10, Caculator.add("1,2,3,4"));
+    }
+
+    @Test
+    public void TestHavingNewLine() throws Exception {
+        assertEquals(6, Caculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void TestHavingDifferenceDelimiter() throws Exception {
+        assertEquals(3, Caculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void TestRaiseException() {
+        try {
+            Caculator.add("//;\n1;-2");
+            fail("Exception expected");
+        } catch (Exception e) {
+
+        }
+    }
+
+    @Test
+    public void TestMessageException() {
+        try {
+            Caculator.add("//;\n1;-2");
+            fail("Exception expected");
+        } catch (Exception e) {
+            assertEquals(e.getMessage(), "negatives not allowed -2");
+
+        }
+    }
+
+    @Test
+    public void TestIgnoreMoreThan1000() throws Exception {
+        assertEquals(2, Caculator.add("//;\n1001;2"));
+    }
+
+    @Test
+    public void TestDelimiterAnyLength() throws Exception {
+        assertEquals(6, Caculator.add("//[***]\n1***2***3"));
+    }
+
+    @Test
+    public void TestMutipleDelimiter() throws Exception {
+        assertEquals(6, Caculator.add("//[*][%]\n1*2%3"));
+    }
+
+    @Test
+    public void TestMutipleDelimiterAnyLength() throws Exception {
+        assertEquals(6, Caculator.add("//[**][%%]\n1**2%%3"));
+    }
+}
