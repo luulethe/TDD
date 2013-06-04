f61c07bafa400803267598183e69610a5a43edac
luult_54 <luulethe@gmail.com>
2013-06-04 13:36:08 +0700

    test case 2 green

--- TestCaculator4-5/src/main/java/Calculator.java
+++ TestCaculator4-5/src/main/java/Calculator.java
@@ -6,7 +6,9 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Calculator {
-    public static int add(String s) {
-        return 0;  //To change body of created methods use File | Settings | File Templates.
+    public static int add(String text) {
+        if (text.equals(""))
+            return 0;
+        return Integer.parseInt(text);  //To change body of created methods use File | Settings | File Templates.
     }
 }
