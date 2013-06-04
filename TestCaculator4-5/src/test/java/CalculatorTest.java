6055dd0a2e5dfd2ec18e165da68496a705029b9c
luult_54 <luulethe@gmail.com>
2013-06-04 13:43:39 +0700

    test case 6 red

--- TestCaculator4-5/src/test/java/CalculatorTest.java
+++ TestCaculator4-5/src/test/java/CalculatorTest.java
@@ -34,4 +34,9 @@ public class CalculatorTest {
     public void testHavingNewLine() {
         assertEquals(Calculator.add("1\n2,3"), 6);
     }
+
+    @Test
+    public void testHavingDifferenceDelimiter() {
+        assertEquals(Calculator.add("//;\n1;2"), 3);
+    }
 }
