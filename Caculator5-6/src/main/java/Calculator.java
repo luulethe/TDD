e2951d425531f036b2ac8af818b22ec5480ea5cf
luult_54 <luulethe@gmail.com>
2013-06-05 13:54:19 +0700

    clean

--- Caculator5-6/src/main/java/Calculator.java
+++ Caculator5-6/src/main/java/Calculator.java
@@ -9,17 +9,30 @@ public class Calculator {
     public static int add(String text) {
         if (text.equals(""))
             return 0;
-        String delimiterString = ",|\n";
-        if (text.startsWith("//"))
-            delimiterString = text.substring(2,3) + "|\n";
-        String textNumber = text;
-        if (text.startsWith("//"))
-            textNumber = text.split("\n")[1];
-        String[] listNumber = textNumber.split(delimiterString);
+        String delimiterString = getDelimiter(text);
+        String[] listNumber = getListNumber(text, delimiterString);
+        return sumList(listNumber);  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static int sumList(String[] listNumber) {
         int sum = 0;
-        for(String s : listNumber) {
+        for (String s : listNumber) {
             sum += Integer.parseInt(s);
         }
         return sum;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    private static String[] getListNumber(String text, String delimiterString) {
+        String textNumber = text;
+        if (text.startsWith("//"))
+            textNumber = text.split("\n")[1];
+        return textNumber.split(delimiterString);
+    }
+
+    private static String getDelimiter(String text) {
+        String delimiterString = ",|\n";
+        if (text.startsWith("//"))
+            delimiterString = text.substring(2, 3) + "|\n";
+        return delimiterString;
     }
 }
