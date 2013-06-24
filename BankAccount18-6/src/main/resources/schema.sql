f3cd00e6db5c59ff4b10ec76ae04de0f8b3a3c01
luult_54 <luulethe@gmail.com>
2013-06-24 11:12:24 +0700

    commit

---
+++ BankAccount18-6/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table if not exists SAVINGS_ACCOUNT (
+  account_number varchar,
+  balance double,
+  open_time_stamp int,
+)
---
+++ BankAccount18-6/src/test/java/TestBankAccountDao.java
@@ -0,0 +1,80 @@
+import dao.BankAccountDao;
+import entity.BankAccountEntity;
+import org.dbunit.IDatabaseTester;
+import org.dbunit.JdbcDatabaseTester;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.tools.RunScript;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.nio.charset.Charset;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luult
+ * Date: 6/23/13
+ * Time: 8:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestBankAccountDao {
+    //using H2 so that we can create in-memory database for testing
+    // without having to install any DBMS software
+    private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
+    private static final String JDBC_URL = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
+    private static final String USER = "sa";
+    private static final String PASSWORD = "";
+    private static final double e = 0.0000001;
+
+    @BeforeClass
+    public static void createSchema() throws Exception {
+        String schemaFileName = System.class.getResource("/schema.sql").toString().substring(6);
+        String temp = System.class.getResource("/schema.sql").toString();
+        System.out.println(schemaFileName);
+        System.out.println(temp);
+        RunScript.execute(JDBC_URL, USER, PASSWORD, schemaFileName, Charset.forName("UTF8"), false);
+    }
+
+    // populate the table with test data
+    @Before
+    public void importDataSet() throws Exception {
+        IDataSet dataSet = readDataSet();  // read data from xml file
+        cleanlyInsert(dataSet);  // empty the db and insert data
+    }
+
+    private void cleanlyInsert(IDataSet dataSet) throws Exception {
+        IDatabaseTester databaseTester = new JdbcDatabaseTester(JDBC_DRIVER, JDBC_URL, USER, PASSWORD);
+        databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+        databaseTester.setDataSet(dataSet);
+        databaseTester.onSetup();
+    }
+
+    private IDataSet readDataSet() throws Exception {
+        return new FlatXmlDataSetBuilder().build(System.class.getResource("/dataset.xml"));
+    }
+
+    @Test
+    public void testFindByAccountNumber() throws Exception {
+        BankAccountDao bankAccountDao = new BankAccountDao(dataSource());
+        BankAccountEntity account = bankAccountDao.getAccount("0123456789");
+
+        assertEquals("0123456789", account.getAccountNumber());
+        assertEquals(100, account.getBalance(), e);
+        assertEquals(12345678, account.getOpenTimeStamp());
+    }
+
+    private DataSource dataSource() {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setURL(JDBC_URL);
+        dataSource.setUser(USER);
+        dataSource.setPassword(PASSWORD);
+        return dataSource;
+    }
+
+}
