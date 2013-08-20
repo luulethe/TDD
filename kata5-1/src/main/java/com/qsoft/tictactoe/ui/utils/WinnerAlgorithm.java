c55c9c498a3832f4962f420ba6e46acc632645e6
luult_54 <luulethe@gmail.com>
2013-08-20 08:24:13 +0700

    Green Test winner In column

--- kata5-1/src/main/java/com/qsoft/tictactoe/ui/utils/WinnerAlgorithm.java
+++ kata5-1/src/main/java/com/qsoft/tictactoe/ui/utils/WinnerAlgorithm.java
@@ -9,6 +9,20 @@ public class WinnerAlgorithm
 {
     public static String check(String[] arrayText)
     {
-        return "X";
+        for (int i = 0; i < 3; i++)
+        {
+            if (arrayText[3 * i].equals(arrayText[3 * i + 1]) && arrayText[3 * i + 1].equals(arrayText[3 * i + 2]))
+            {
+                return arrayText[3 * i];
+            }
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            if (arrayText[i].equals(arrayText[i + 3]) && arrayText[i + 3].equals(arrayText[i + 6]))
+            {
+                return arrayText[i];
+            }
+        }
+        return "";
     }
 }
