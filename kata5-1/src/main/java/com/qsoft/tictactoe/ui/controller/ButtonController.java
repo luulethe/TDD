63f2f13e0e63a53c983b21d34ac867f6fb5bae30
Phạm Trần Trung <trungptdhcn@gmail.com>
2013-08-19 19:44:34 -0700

    Green acceptionTest ShowHistoryWhenNoGameHappen

--- kata5-1/src/main/java/com/qsoft/tictactoe/ui/controller/ButtonController.java
+++ kata5-1/src/main/java/com/qsoft/tictactoe/ui/controller/ButtonController.java
@@ -5,6 +5,7 @@ import com.qsoft.tictactoe.ui.view.MainWindow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -42,6 +43,17 @@ public class ButtonController implements ActionListener
         else if (e.getActionCommand().equals("History"))
         {
             frameHistory.setContenpanel();
+            DefaultTableModel tableModel = new DefaultTableModel();
+            createTable(tableModel);
         }
+    }
+    public void createTable(DefaultTableModel tableModel)
+    {
+        tableModel.addColumn("Index");
+        tableModel.addColumn("First player");
+        tableModel.addColumn("Winner");
+        tableModel.addColumn("Steps");
+        frameHistory.getHistoryTable().setModel(tableModel);
+
     }
 }
